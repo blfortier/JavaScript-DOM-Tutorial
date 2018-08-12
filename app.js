75d1e99576984af5d9ac94cbe1f09593b0474e66
Brittney <brittfort34@gmail.com>
2018-08-12 17:44:03 +0000

    Interacting with Forms

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ list.addEventListener('click', function(e) {
   } 
     
 });
+
+// add book-list
+const addForm = document.forms['add-book'];
+
+// attach eventListener to form
+addForm.addEventListener('submit', function(e) {
+   e.preventDefault();
+   const value = addForm.querySelector('input[type="text"]').value;
+   console.log(value);
+    
+});
