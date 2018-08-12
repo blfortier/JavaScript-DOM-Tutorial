eba684d0d6ab30cc29003638302636f36252766d
Brittney <brittfort34@gmail.com>
2018-08-12 17:32:40 +0000

    Event Bubbling

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const listItems = document.querySelectorAll('#book-list ul li');
+const list = document.querySelector('#book-list ul');
 
-Array.from(listItems).forEach(function(item) {
-    item.addEventListener('click', (e) => {
-        const li = e.target.parentElement;
-        
-        console.log('child element to remove:', li);
-        console.log('parent element to remove child from:', li.parentElement);
-        li.parentNode.removeChild(li);
-    });
+// delete books
+list.addEventListener('click', function(e) {
+  if(e.target.className == 'delete') {
+      const li = e.target.parentElement;
+      list.removeChild(li);
+  } 
+    
 });
