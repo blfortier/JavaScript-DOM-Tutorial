f9fc734b3c6a342e2268332a05aad042671a3465
Brittney <brittfort34@gmail.com>
2018-08-12 17:52:35 +0000

    Creating Elements

--- app.js
+++ app.js
@@ -9,13 +9,26 @@ list.addEventListener('click', function(e) {
     
 });
 
-// add book-list
+// add books
 const addForm = document.forms['add-book'];
 
 // attach eventListener to form
 addForm.addEventListener('submit', function(e) {
    e.preventDefault();
    const value = addForm.querySelector('input[type="text"]').value;
-   console.log(value);
+   
+   // create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
     
+    // add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+    
+    // append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
 });
+
